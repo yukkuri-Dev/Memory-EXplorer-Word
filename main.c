2dda4ebcd0571c0e69c155b5ccf83af1d8b8498c
yukkuri-Dev <76156149+yukkuri-Dev@users.noreply.github.com>
2025-08-14 13:56:57 +0900

    test2

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#include <graphics/text.h>
+#include <sh4a/input/keypad.h>
+#include <syscalls/syscalls.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void main(void) {
     	while(1) {
 		if (get_key_pressed(KEY_POWER))
