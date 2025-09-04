de68bc1f2d000ba362c0bb0ed64cf698c77033a2
CloverTech1105 <76156149+yukkuri-Dev@users.noreply.github.com>
2025-09-04 13:38:50 +0900

    main関数の実装を整理し、不要なコードを削除してファイル作成と書き込み処理を追加

--- main.c
+++ main.c
@@ -1,45 +1,38 @@
-#include <graphics/text.h>
-#include <sh4a/input/keypad.h>
 #include <syscalls/syscalls.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <rc/rc.h>
 
-static char *media[2] = {
-	"\\\\drv0\\",
-	"\\\\crd0\\"
-};
-
-void main(void) {
-	static char *sramfile = NULL;
-
-	//char *buf;
-	//char id[10];
-	//unsigned long drv;
-	//sys_dict_info(&drv,id);
-	//buf = malloc(50);
-	//sprintf(buf, "%s:%s%s\\_USER\\", media[drv], media[drv], id);
-	//rc_setvar("rcpath", 1,buf);
-	//sprintf(buf, "%sROMS\\", media[drv]);
-	//rc_setvar("savedir", 1, buf);
-	//rc_setvar("romdir", 1, buf);
-
+int main(void) {
     unsigned char data[26] = {
-        0x48, 0x45, 0x4C, 0x4C, 0x4F, 0x20, 0x57, 0x4F,
-        0x52, 0x4C, 0x44, 0x0D, 0x0A, 0x50, 0x4F, 0x43,
-        0x20, 0x49, 0x53, 0x20, 0x57, 0x4F, 0x52, 0x4B,
-        0x53, 0x21
+        0x48,0x45,0x4C,0x4C,0x4F,0x20,0x57,0x4F,
+        0x52,0x4C,0x44,0x0D,0x0A,0x50,0x4F,0x43,
+        0x20,0x49,0x53,0x20,0x57,0x4F,0x52,0x4B,
+        0x53,0x21
     };
 
+    // 保存先パスを自分で作る
+    char *filename = malloc(128);
+    if (!filename) return 1;
 
+    // 例: drv0 直下に保存
+    strcpy(filename, "\\\\drv0\\poop.txt");
 
+    // ファイル作成 (WRITE モード)
+    int fd = sys_create(filename, FILE_WR);
+    if (fd < 0) {
+        free(filename);
+        return 1;
+    }
 
-	sramfile = "test.txt";
+    // 書き込み
+    sys_write(fd, data, sizeof(data));
 
-	FILE *f = fopen(sramfile,"wb");
-	size_t written = fwrite(data,1,sizeof(data),f);
-	fclose(f);
-    while(1) {
-	}
+    // ファイルを閉じる
+    sys_close(fd);
+
+    free(filename);
+
+    while(1) {} // 終了しない
+
+    return 0;
 }
