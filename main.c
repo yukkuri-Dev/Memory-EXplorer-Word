6012ea166346c7c2a331a61b9078571f88f2d952
CloverTech1105 <76156149+yukkuri-Dev@users.noreply.github.com>
2025-09-04 13:28:40 +0900

    disable code

--- main.c
+++ main.c
@@ -14,16 +14,16 @@ static char *media[2] = {
 void main(void) {
 	static char *sramfile = NULL;
 
-	char *buf;
-	char id[10];
-	unsigned long drv;
-	sys_dict_info(&drv,id);
-	buf = malloc(50);
-	sprintf(buf, "%s:%s%s\\_USER\\", media[drv], media[drv], id);
-	rc_setvar("rcpath", 1,buf);
-	sprintf(buf, "%sROMS\\", media[drv]);
-	rc_setvar("savedir", 1, buf);
-	rc_setvar("romdir", 1, buf);
+	//char *buf;
+	//char id[10];
+	//unsigned long drv;
+	//sys_dict_info(&drv,id);
+	//buf = malloc(50);
+	//sprintf(buf, "%s:%s%s\\_USER\\", media[drv], media[drv], id);
+	//rc_setvar("rcpath", 1,buf);
+	//sprintf(buf, "%sROMS\\", media[drv]);
+	//rc_setvar("savedir", 1, buf);
+	//rc_setvar("romdir", 1, buf);
 
     unsigned char data[26] = {
         0x48, 0x45, 0x4C, 0x4C, 0x4F, 0x20, 0x57, 0x4F,
